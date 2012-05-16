2ab32c3490d2551fbad5dd7962091e09518b8635
Nick Stupich <nick.stupich@gmail.com>
2012-05-16 17:31:25 -0400

    more work with different sampling frequencies and window sizes

--- v1.py
+++ v1.py
@@ -2,8 +2,10 @@ from numpy.fft import rfft as fourier
 from numpy import absolute
 from math import sin, pi, log10
 from matplotlib import pylab
+from numpy import float32 as dtype
 
-testNums = [1, 2, 2, 1]
+numSamples = 128
+sampleFreq = 768
 
 def generateTimeDomain(times, frequencies):
 	result = [0] * len(times)
@@ -12,6 +14,7 @@ def generateTimeDomain(times, frequencies):
 		for (freq, amp) in frequencies:
 			result[i] += sin(2.0 * pi * t * freq) * amp
 	
+	result = map(dtype, result)
 	return result
 	
 def generateTimes(sampleFreq, numSamples):
@@ -24,21 +27,65 @@ def generateFFTFrequencies(sampleFreq, numSamples):
 def toDb(x):
 	return 20 * log10(x)
 	
-def main():
-	numSamples = 256
-	sampleFreq = 1024
-
+def plot(x, y, semilogx = False):
+	if semilogx:
+		pylab.semilogx(x, y)
+	else:
+		pylab.plot(x, y)
+		
+	pylab.grid(True)
+	pylab.show()
+	
+def compareFrequencies():
 	times = generateTimes(sampleFreq, numSamples)
-	timeData = generateTimeDomain(times, [(81, 1)])#, (90, 0.5)])
+	signal = (80.0, 0.1)
+	coherent = (50.0, 1.0)
+	incoherent = (51.0, 1.0)
+	highFNoise = (500.0, 0.01)
+	timeData = generateTimeDomain(times, [signal, coherent, highFNoise])
+	timeData2 = generateTimeDomain(times, [signal, incoherent, highFNoise])
+	timeData3 = generateTimeDomain(times, [signal, highFNoise])
 	
-	pylab.plot(times, timeData); pylab.show()
+	#timeData = generateTimeDomain(times, [(60.0, 1.0)])
+	#timeData2 = generateTimeDomain(times, [(61.0, 1.0)])
 	
-	freqData = map(absolute, fourier(timeData))
-	freqDb = map(toDb, freqData)
+	roi = (0, 20)
+	
+	freqData = map(toDb, map(dtype, map(absolute, fourier(timeData))))[roi[0]:roi[1]]
+	freqData2 = map(toDb, map(dtype, map(absolute, fourier(timeData2))))[roi[0]:roi[1]]
+	freqData3 = map(toDb, map(dtype, map(absolute, fourier(timeData3))))[roi[0]:roi[1]]
+	
+	frequencies = generateFFTFrequencies(sampleFreq, numSamples)[roi[0]:roi[1]]
+	
+	#pylab.subplot(111)
+	pylab.plot(frequencies, freqData)
+	
+	#pylab.subplot(112)
+	pylab.plot(frequencies, freqData2)
+	
+	pylab.plot(frequencies, freqData3)
+	
+	pylab.grid(True)
+	pylab.show()
+	
+def test1():
+	times = generateTimes(sampleFreq, numSamples)
+	timeData = generateTimeDomain(times, [(50.0, 1.0)])
+	
+	#plot(times, timeData)
+	
+	freqData = map(toDb, map(dtype, map(absolute, fourier(timeData))))
 	
 	frequencies = generateFFTFrequencies(sampleFreq, numSamples)
 	print 'bin size: %s' % frequencies[1]
-	pylab.plot(frequencies, freqDb); pylab.show()
+	
+	#plot(frequencies, freqData, False)
+	plot(frequencies[:15], freqData[:15], False)
+	
+def main():
+	
+	#compareFrequencies()
+	test1()
 	
 if __name__ == "__main__":
 	main()
